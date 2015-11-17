713eafad51077ef3c77797674c8d8a98ff107e68
Adan De Santiago <adan1995a@gmail.com>
2015-11-16 22:24:55 -0600

    resuelto algoritmo a posfijo

---
+++ expresionesAritmeticas/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace expresionesAritmeticas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        String expresionAritmetica;
+        String solucion;
+        Posfijo pos = new Posfijo();
+
+        private void btnresolver_Click(object sender, EventArgs e)
+        {
+            expresionAritmetica = txtAritmetica.Text;
+            solucion = pos.postfijo(expresionAritmetica);
+            txtSolucion.Text = solucion;
+        }
+    }
+}
---
+++ expresionesAritmeticas/Posfijo.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace expresionesAritmeticas
+{
+    enum prioridad { numero, parentesisIzquierdo, parentesisDerecho, sumaResta, multiplicacionDivision, potencia };
+    //los enumeradores asignan un valor numerico a cada miembro comenzando con cero
+    class Posfijo
+    {
+        int tope;
+        Regex reg = new Regex("[^0-9]");
+        Stack<string> pila = new Stack<string>();
+        string notacionPostfija;
+
+        public string postfijo(string infijo)
+        {
+            tope = 0;
+                for (int i = 0; i < infijo.Length; i++)
+                {
+                    if (infijo[i] == '(')
+                    {
+                        tope++;
+                        pila.Push(Convert.ToString( infijo[i]));
+                    }
+                    else
+                    {
+                        if (infijo[i] == ')')
+                        {
+                            while (Convert.ToChar(pila.Peek()) != '(')// podria ser igual la condicion
+                            {
+                                tope--;
+                                notacionPostfija += pila.Pop();//podria ser pila.pop()
+                            }
+                            pila.Pop();
+                            tope--;
+                        }
+                        else
+                        {
+                            if (infijo[i] >= '0' && infijo[i] <= '9')// si es un operando
+                            {
+                                notacionPostfija += infijo[i];
+                            }
+                            else { // el char es un operador
+                                if (pila.Count == 0)
+                                {
+                                    tope++;
+                                    pila.Push(Convert.ToString(infijo[i]));
+                                }
+                                else
+                                {   // ahora debemos revisar la prioridad de operadores
+
+                                    prioridad simboloActual = prioridadOperador(Convert.ToChar(infijo[i]));
+                                    //llamamos metodo que define prioridad de operadores
+                                    if (prioridadOperador(Convert.ToChar(pila.Peek())) >= simboloActual)
+                                    {
+                                        while (pila.Count != 0)
+                                        {
+                                            tope--;
+                                            notacionPostfija += pila.Pop();
+                                        }
+                                    }
+                                    tope++;
+                                    pila.Push(Convert.ToString(infijo[i]));
+                                }       
+                            }// fin else
+                        }
+                    }
+                }
+                if (pila.Count != 0)
+                {
+                    while (pila.Count != 0)
+                    {
+                        notacionPostfija += pila.Pop();
+                    }
+                }
+                return notacionPostfija;
+            }
+
+        
+
+        
+
+        public prioridad prioridadOperador(char operador)
+        {
+            prioridad operacion;
+            switch (operador)
+            {
+                case '+':
+                    operacion = prioridad.sumaResta;
+                    break;
+                case '-':
+                    operacion = prioridad.sumaResta;
+                    break;
+                case '*':
+                    operacion = prioridad.multiplicacionDivision;
+                    break;
+                case '/':
+                    operacion = prioridad.multiplicacionDivision; 
+                    break;
+                case '(':
+                    operacion = prioridad.parentesisIzquierdo;
+                    break;
+                case ')':
+                    operacion = prioridad.parentesisDerecho;
+                    break;
+                case '^':
+                    operacion = prioridad.potencia;
+                    break;
+                default:
+                    operacion = prioridad.numero;
+                    break;
+            }
+            return operacion;
+        }
+       
+    }
+}
